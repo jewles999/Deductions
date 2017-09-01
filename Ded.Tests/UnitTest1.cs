d38f3c9c1f407d582be40fa4a9d5e7d6f91db439
curiousCat999 <curiousCat999@yahoo.com>
2017-09-01 16:30:49 -0400

    Add project files.

---
+++ Ded.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ded.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
